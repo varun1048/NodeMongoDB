b118f0cac8f084e4bd013b9ced0731b977116767
varun <59528216+varun1048@users.noreply.github.com>
2020-11-15 20:17:32 +0530

    d

--- index.js
+++ index.js
@@ -15,52 +15,91 @@ mongoclient.connect(url,{useUnifiedTopology: true},(err,data)=>{
     //     data.close()
     // })
 
-// let myobj = [
-//         { name: 'John', address: 'Highway 71'},
-//         { name: 'Peter', address: 'Lowstreet 4'},
-//         { name: 'Amy', address: 'Apple st 652'},
-//         { name: 'Hannah', address: 'Mountain 21'},
-//         { name: 'Michael', address: 'Valley 345'},
-//         { name: 'Sandy', address: 'Ocean blvd 2'},
-//         { name: 'Betty', address: 'Green Grass 1'},
-//         { name: 'Richard', address: 'Sky st 331'},
-//         { name: 'Susan', address: 'One way 98'},
-//         { name: 'Vicky', address: 'Yellow Garden 2'},
-//         { name: 'Ben', address: 'Park Lane 38'},
-//         { name: 'William', address: 'Central st 954'},
-//         { name: 'Chuck', address: 'Main Road 989'},
-//         { name: 'Viola', address: 'Sideway 1633'}
-//       ];
+function createdata(){
+
     
-
-//     db.collection("customers").insertMany( myobj ,(err,inner)=>{
-//         if(err) throw err
-//         // log(inner.ops)
-
-//         console.log("Number of documents inserted: " + inner);
-//         data.close()
-//     })
-// let datas
-
-// db.collection('customers').find({},{ projection: { _id:0,name: 1, address: 1 } }).toArray((err,values)=>{
-//     if(err) throw err
-//     log(values)
-//     // datas.push(values)
+    let myobj = [
+                { name: 'John', address: 'Highway 71'},
+                { name: 'Peter', address: 'Lowstreet 4'},
+                { name: 'Amy', address: 'Apple st 652'},
+                { name: 'Hannah', address: 'Mountain 21'},
+                { name: 'Michael', address: 'Valley 345'},
+                { name: 'Sandy', address: 'Ocean blvd 2'},
+                { name: 'Betty', address: 'Green Grass 1'},
+                { name: 'Richard', address: 'Sky st 331'},
+                { name: 'Susan', address: 'One way 98'},
+                { name: 'Vicky', address: 'Yellow Garden 2'},
+                { name: 'Ben', address: 'Park Lane 38'},
+                { name: 'William', address: 'Central st 954'},
+                { name: 'Chuck', address: 'Main Road 989'},
+                { name: 'Viola', address: 'Sideway 1633'}
+              ];
+        
+        
+            db.collection("customers").insertMany( myobj ,(err,inner)=>{
+                    if(err) throw err
+                    // log(inner.ops)
+            
+                    console.log("Number of documents inserted: " + inner);
+                    data.close()
+                })
+            let datas
+}
+// createdata()
+            
+            // db.collection('customers').find({},{ projection: { _id:0,name: 1, address: 1 } }).toArray((err,values)=>{
+                    // if(err) throw err
+                //     log(values)
+                //     // datas.push(values)
 //     // log(datas)
 //     data.close()
 // })
 // log(datas)
 
-db.collection('customers').find({},{}).toArray((err,datas)=>{
-    if(err) throw err
+    // db.collection('customers').find({name: 'John'},{projection:{_id:0 ,name:0, addreass:0} }).toArray((err,datas)=>{
+    
 
-    log(datas)
-    data.close()
-})
+    // db.collection('customers').find().sort({name:-1}).toArray((err,datas)=>{
+    //     if(err) throw err
+    //     log(datas)
+    //     data.close()
+    // })
+//     let deleting={}
+// db.collection('customers').deleteOne(deleting,(err,inner)=>{
+// data.close()
+// })
 
+// db.collection('customers').drop((err,obj)=>log(obj))
+// 
+// db.collection('customers').deleteOne({name: /^Viola/},(err,datas)=>{data.close()})
+
+// let deletingmany={name:/^/}
+// db.collection('customers').deleteMany(deletingmany,(err,datas)=>{
+    //     log(datas.result.n)
+    //     data.close()})
+    
+    let allfunc=(err,datas)=>{
+        if(err) throw err
+        data.close()
+    }
+    let insert  = {name:"varun",addreass:"karapakkam",age:23}
+    
+    // db.collection('customers').insertOne(insert,allfunc)
+    let set = {$set:{addreass:"chennai"}}
+    let deleteone={address:/^address/}
+    db.collection('customers').deleteOne(deleteone,allfunc)
+    // db.collection('customers').updateOne({name:'varun'},set,allfunc)
+    
+    // createdata()
+    db.collection('customers').find({},{projection:{_id:0}}).toArray((err,datas)=>{
+        log(datas)
+        data.close()
+    })
 
 
 //         if(err) throw err
 // data.close()
+
+
 
 })
--- test.js
+++ test.js
@@ -1,12 +1,17 @@
+const express = require('express')
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
-
+let app = express()
 MongoClient.connect(url,{useUnifiedTopology: true}, function(err, db) {
   if (err) throw err;
   var dbo = db.db("varundb");
-  dbo.collection("customers").find({}).toArray(function(err, result) {
+  dbo.collection("customers").find({},{projection:{_id:0}}).toArray(function(err, result) {
     if (err) throw err;
-    console.log(result);
+    app.get('/',(q,s)=>{
+      s.send(`<h1>${result}<h1>`)
+    })
+    // console.log(result);
     db.close();
   });
 }); 
+app.listen(3000,console.log("port 3000"))
